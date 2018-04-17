1a3d79539d1994d14dcb8261a441c4b2f202e143
David Moore <hey@iamdavidmoore.com>
2018-04-17 16:34:34 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(arg="Swuawk!")
-  puts arg
+def parrot(arg="Squawk!")
+  output = puts arg
+  return output
 end
